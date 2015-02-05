9cc523acb4310d3d35777cc8265c3dd15f27fa90
RyuKata <ryuichi.pad>
2015-02-05 19:31:10 +0900

    Add In Out1

--- sample.rb
+++ sample.rb
@@ -1,1 +1,23 @@
+y = gets.to_i
+m = gets.to_i
+d = gets.to_i
+# h = calculation(y,m,d)
 
+case h
+when 1
+	p "日曜日"
+when 2
+	p "月曜日"
+when 3
+	p "火曜日"
+when 4
+	p "水曜日"
+when 5
+	p "木曜日"
+when 6
+	p "金曜日"
+when 7
+	p "土曜日"
+else
+	p "error"
+end
